e8e6ab1096ee78ee6154b578c9a92adfe12b7ada
rjzupkoii <rjzii@yahoo.com>
2020-08-26 15:29:20 -0400

    Updated to run with variable base

--- cluster/run.sh
+++ cluster/run.sh
@@ -3,16 +3,17 @@
 # The number to limit the processes to
 LIMIT=90
 
-for i in `seq 0.01 0.01 1.25`;
-do
-  # Get the current job count, note the overcount due to the delay.
-  # Wait if there are currently too many jobs
-  while [ `qstat -u rbz5100 | grep rbz5100 | wc -l` -gt $LIMIT ]
-  do
-    sleep 10s
-  done
+for adjustment in 0.2 0.3 0.4 0.5; do
+  for i in `seq 0.01 0.01 1.25`; do
+    # Get the current job count, note the overcount due to the delay.
+    # Wait if there are currently too many jobs
+    while [ `qstat -u rbz5100 | grep rbz5100 | wc -l` -gt $LIMIT ]
+    do
+      sleep 10s
+    done
 
-  sed 's/#BETA#/'"$i"'/g' mmc.yml > mmc-0.30-$i.yml
-  sed 's/#BETA#/'"$i"'/g' template.job > 0.30-$i.pbs
-  qsub 0.30-$i.pbs
+    sed 's/#BETA#/'"$i"'/g' mmc.yml > mmc-0.30-$i.yml
+    sed 's/#BETA#/'"$i"'/g' template.job > 0.30-$i.pbs
+    qsub 0.30-$i.pbs
+  done
 done
